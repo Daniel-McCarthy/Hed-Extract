596a985a2992e0523ad4bbaa53b136982fc73bcb
UnknownToaster <wererabbbit@yahoo.com>
2017-03-31 16:10:13 -0500

    DirectorySelector Add/Select Functions Added
    -Directory Selector can have directories added to the list by string, or
    array of strings.
    
    -Button on form now sets the return directory before closing the dialog.

--- Source/Hed Extract/DirectorySelector.Designer.cs
+++ Source/Hed Extract/DirectorySelector.Designer.cs
@@ -49,6 +49,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Select Folder to Build";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // DirectorySelector
             // 
