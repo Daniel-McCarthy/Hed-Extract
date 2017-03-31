f399c58cb19694aa75a6041a9fefcd4e99dcc145
UnknownToaster <wererabbbit@yahoo.com>
2017-03-31 17:17:16 -0500

    Revert "DirectorySelector Add/Select Functions Added"
    This reverts commit 596a985a2992e0523ad4bbaa53b136982fc73bcb.

--- Source/Hed Extract/DirectorySelector.Designer.cs
+++ Source/Hed Extract/DirectorySelector.Designer.cs
@@ -49,7 +49,6 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Select Folder to Build";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // DirectorySelector
             // 
--- Source/Hed Extract/DirectorySelector.cs
+++ Source/Hed Extract/DirectorySelector.cs
@@ -12,30 +12,9 @@ namespace Hed_Extract
 {
     public partial class DirectorySelector : Form
     {
-        public string returnDirectory { get; set; }
-
         public DirectorySelector()
         {
             InitializeComponent();
-        }
-
-        public void addDirectory(string directory)
-        {
-            directoryListBox.Items.Add(directory);
-        }
-
-        public void addDirectories(string[] directories)
-        {
-            foreach(string directory in directories)
-            {
-                directoryListBox.Items.Add(directory);
-            }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            returnDirectory = directoryListBox.SelectedItem.ToString();
-            this.Close();
         }
     }
 }
